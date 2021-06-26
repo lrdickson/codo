ac8507e26825ea4113b660a1169ba81e71bee70a
Lyndsey Dickson <lyndsey.dickson@protonmail.com>
2021-06-26 09:00:13 -0400

    build docker images

--- cmd/codoctl/codoctl.go
+++ cmd/codoctl/codoctl.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
+	"os/user"
 	"path/filepath"
 
 	"github.com/spf13/pflag"
@@ -30,6 +32,43 @@ func BuildImage(imagesFolder string, imageName string) {
 		return
 	}
 	fmt.Printf("parsed config: %v\n", imageConfig)
+
+	// Create the Dockerfile text
+	baseImage, configHasBaseImage := imageConfig["base-image"]
+	if ! configHasBaseImage {
+		log.Printf("No base image given for %v\n", imageName)
+		return
+	}
+	dockerfileText := fmt.Sprintf("FROM %v\n", baseImage)
+
+	// Create the Dockerfile
+	user, err := user.Current()
+	if err != nil {
+		log.Printf("failed to get current user: %v\n", err)
+		return
+	}
+	username := user.Username
+	dockerfileDir := filepath.Join("/","tmp", "codo", username, imageName)
+	err = os.MkdirAll(dockerfileDir, 0755)
+	if err != nil {
+		log.Printf("failed to create Dockerfile directory for %v: %v\n", imageName, err)
+		return
+	}
+	dockerfileLocation := filepath.Join(dockerfileDir, "Dockerfile")
+	err = os.WriteFile(dockerfileLocation, []byte(dockerfileText), 0666)
+	if err != nil {
+		log.Printf("failed to write Dockerfile for %v: %v\n", imageName, err)
+		return
+	}
+
+	// Build the image
+	codoImageName := fmt.Sprintf("codo-%v-%v", username, imageName)
+	buildCommand := exec.Command("sudo", "docker", "build", "-t", codoImageName, dockerfileDir)
+	err = buildCommand.Run()
+	if err != nil {
+		log.Printf("failed to build image for %v: %v\n", imageName, err)
+		return
+	}
 }
 
 
@@ -50,7 +89,7 @@ func BuildAllImages(configFolder string) {
 
 func main() {
 	// Parse command line arguments
-	buildFlag := pflag.BoolP("build", "b", false, "Build images")
+	buildAllFlag := pflag.BoolP("buildall", "B", false, "Build images")
 	pflag.Parse()
 
 	// Get the config folder
@@ -61,8 +100,8 @@ func main() {
 	configFolder := filepath.Join(home, ".config", "codo")
 
 	// Check if images are to be built
-	fmt.Println(*buildFlag)
-	if *buildFlag {
+	fmt.Println(*buildAllFlag)
+	if *buildAllFlag {
 		BuildAllImages(configFolder)
 	}
 }
