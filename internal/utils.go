b187538670eabb2557057ad9feb9916a8266683d
Lyndsey Dickson <lyndseyrd@gmail.com>
2021-06-28 16:52:25 -0400

    Record the image working directory

--- internal/utils.go
+++ internal/utils.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -43,9 +44,9 @@ func BuildImage(imagesFolder string, imageName string) {
 		log.Printf("failed to read %v Dockerfile: %v\n", imageName, err)
 		return
 	}
-	outputDockerfileText := inputDockerfileText
 
 	// Create the output Dockerfile
+	outputDockerfileText := inputDockerfileText
 	buildDir := filepath.Join("/","tmp", "codo", imageName)
 	err = os.MkdirAll(buildDir, 0755)
 	if err != nil {
@@ -67,6 +68,34 @@ func BuildImage(imagesFolder string, imageName string) {
 		log.Printf("failed to build image for %v: %v\n", imageName, err)
 		return
 	}
+
+	// Get the image working directory
+	command := exec.Command("sudo", "docker", "run",  "--rm", fullImageName, "pwd")
+	var commandOutput bytes.Buffer
+	command.Stdout = &commandOutput
+	err = command.Run()
+	if err != nil {
+		log.Printf("failed to get %v working directory: %v\n", imageName, err)
+		return
+	}
+	imageWorkingDir := commandOutput.String()
+	log.Println(imageWorkingDir)
+
+	// Record the working directory
+	storageDir := GetStorageDir(imageName)
+	stateFile := filepath.Join(storageDir, "state.yaml")
+	imageState := make(map[interface{}]interface{})
+	imageState["working-dir"] = imageWorkingDir
+	imageStateData, err := yaml.Marshal(&imageState)
+	if err != nil {
+		log.Printf("failed to create state data for %v: %v\n", imageName, err)
+		return
+	}
+	err = os.WriteFile(stateFile, imageStateData, 0666)
+	if err != nil {
+		log.Printf("failed to write image state data for %v: %v\n", imageName, err)
+		return
+	}
 }
 
 func GetConfigDir() string {
@@ -82,6 +111,23 @@ func GetFullImageName(imageName string) string {
 	username := GetUsername()
 	return fmt.Sprintf("codo-%v-%v", username, imageName)
 }
+
+func GetStorageDir(imageName string) string {
+	// Get the storage folder
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("Unable to get home directory: %v", err)
+	}
+	storageDir := filepath.Join(home, "codo", imageName)
+
+	// Make sure that the storage folder exists
+	err = os.MkdirAll(storageDir, 0755)
+	if err != nil {
+		log.Fatalf("failed to create storage directory for %v: %v\n", imageName, err)
+	}
+	return storageDir
+}
+
 
 func GetUsername() string {
 	user, err := user.Current()
