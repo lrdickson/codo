e0a95b2cf05b876f1549e2932c5c32d970e3ebac
Lyndsey Dickson <lyndseyrd@gmail.com>
2021-06-28 15:08:23 -0400

    Update BuildImage function and move to internal library

--- cmd/codoctl/codoctl.go
+++ cmd/codoctl/codoctl.go
@@ -2,71 +2,18 @@ package main
 
 import (
 	// Standard library
-	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
-	"os/exec"
 	"path/filepath"
 
 	// 3rd Party
 	"github.com/spf13/pflag"
-	"gopkg.in/yaml.v2"
 
 	// Internal
 	"codo/internal"
 )
 
 
-func BuildImage(imagesFolder string, imageName string) {
-	// Get the contents of the image config file
-	imageFolder := filepath.Join(imagesFolder, imageName)
-	imageConfigFile := filepath.Join(imageFolder, "config.yaml")
-	imageConfigContent, err := ioutil.ReadFile(imageConfigFile)
-	if err != nil {
-		log.Printf("failed to read %v config: %v\n", imageName, err)
-		return
-	}
-
-	// Parse the contents
-	imageConfig := make(map[interface{}]interface{})
-	err = yaml.Unmarshal(imageConfigContent, &imageConfig)
-	if err != nil {
-		log.Printf("failed to read %v config: %v\n", imageName, err)
-		return
-	}
-
-	// Create the Dockerfile text
-	baseImage, configHasBaseImage := imageConfig["base-image"]
-	if ! configHasBaseImage {
-		log.Printf("No base image given for %v\n", imageName)
-		return
-	}
-	dockerfileText := fmt.Sprintf("FROM %v\n", baseImage)
-
-	// Create the Dockerfile
-	dockerfileDir := filepath.Join("/","tmp", "codo", imageName)
-	err = os.MkdirAll(dockerfileDir, 0755)
-	if err != nil {
-		log.Printf("failed to create Dockerfile directory for %v: %v\n", imageName, err)
-		return
-	}
-	dockerfileLocation := filepath.Join(dockerfileDir, "Dockerfile")
-	err = os.WriteFile(dockerfileLocation, []byte(dockerfileText), 0666)
-	if err != nil {
-		log.Printf("failed to write Dockerfile for %v: %v\n", imageName, err)
-		return
-	}
-
-	// Build the image
-	fullImageName := internal.GetFullImageName(imageName)
-	buildCommand := exec.Command("sudo", "docker", "build", "-t", fullImageName, dockerfileDir)
-	err = buildCommand.Run()
-	if err != nil {
-		log.Printf("failed to build image for %v: %v\n", imageName, err)
-		return
-	}
-}
 
 
 func BuildAllImages() {
@@ -81,7 +28,7 @@ func BuildAllImages() {
 	}
 	for _, content := range imagesFolderContents {
 		if content.IsDir() {
-			BuildImage(imagesFolder, content.Name())
+			internal.BuildImage(imagesFolder, content.Name())
 		}
 	}
 }
--- internal/utils.go
+++ internal/utils.go
@@ -2,11 +2,72 @@ package internal
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"os/user"
 	"path/filepath"
+
+	// 3rd party
+	"gopkg.in/yaml.v2"
 )
+
+func BuildImage(imagesFolder string, imageName string) {
+	// Get the contents of the image config file
+	imageFolder := filepath.Join(imagesFolder, imageName)
+	imageConfigFile := filepath.Join(imageFolder, "config.yaml")
+	imageConfigContent, err := ioutil.ReadFile(imageConfigFile)
+	if err != nil {
+		log.Printf("failed to read %v config: %v\n", imageName, err)
+		return
+	}
+
+	// Parse the contents
+	imageConfig := make(map[interface{}]interface{})
+	err = yaml.Unmarshal(imageConfigContent, &imageConfig)
+	if err != nil {
+		log.Printf("failed to read %v config: %v\n", imageName, err)
+		return
+	}
+	passGui, configHasPassGui := imageConfig["pass-gui"]
+	if ! configHasPassGui {
+		passGui = false
+	}
+	log.Println(passGui)
+
+	// Read the input docker file
+	inputDockerfile := filepath.Join(imageFolder, "Dockerfile")
+	inputDockerfileText, err := ioutil.ReadFile(inputDockerfile)
+	if err != nil {
+		log.Printf("failed to read %v Dockerfile: %v\n", imageName, err)
+		return
+	}
+	outputDockerfileText := inputDockerfileText
+
+	// Create the output Dockerfile
+	buildDir := filepath.Join("/","tmp", "codo", imageName)
+	err = os.MkdirAll(buildDir, 0755)
+	if err != nil {
+		log.Printf("failed to create Dockerfile directory for %v: %v\n", imageName, err)
+		return
+	}
+	outputDockerfile := filepath.Join(buildDir, "Dockerfile")
+	err = os.WriteFile(outputDockerfile, []byte(outputDockerfileText), 0666)
+	if err != nil {
+		log.Printf("failed to write Dockerfile for %v: %v\n", imageName, err)
+		return
+	}
+
+	// Build the image
+	fullImageName := GetFullImageName(imageName)
+	buildCommand := exec.Command("sudo", "docker", "build", "-t", fullImageName, buildDir)
+	err = buildCommand.Run()
+	if err != nil {
+		log.Printf("failed to build image for %v: %v\n", imageName, err)
+		return
+	}
+}
 
 func GetConfigDir() string {
 	// Get the config folder
