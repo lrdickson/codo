a4c35aa6ae375928f4e30d25da76db0321aec28b
Lyndsey Dickson <lyndseyrd@gmail.com>
2021-07-06 16:35:31 -0400

    Fix being unable to pass flags with command

--- src/config.rs
+++ src/config.rs
@@ -8,10 +8,11 @@ const DEFAULT_CODO_CONFIG: &str = "
 default-image: fedora
 ";
 
-pub fn get_codo_config() -> Vec<Yaml> {
+pub fn get_codo_config() -> Yaml {
     // Get the default codo config as a fallback
     let default_codo_config = YamlLoader::load_from_str(DEFAULT_CODO_CONFIG)
-        .expect("Failed to parse default codo config.");
+        .expect("Failed to parse default codo config.")[0]
+        .to_owned();
 
     
     return default_codo_config;
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,26 @@ use log::debug;
 // Internal
 mod config;
 
+fn get_flag_value<'a>(matches: &'a clap::ArgMatches, input_command_index: usize, flag_name: &'a str, default_value: &'a str) -> &'a str  {
+    match matches.index_of(flag_name) {
+        Some(index) => {
+            if index < input_command_index {
+                //matches.value_of(flag_name).expect("Failed to get flag")
+                match matches.value_of(flag_name) {
+                    Some(value) => value,
+                    None => {
+                        error!("Failed to get value for {:?}", flag_name);
+                        default_value
+                    }
+                }
+            } else {
+                default_value
+            }
+        },
+        None => default_value
+    }
+}
+
 fn main() {
     // Start the enviromental logger
     env_logger::init();
@@ -20,11 +40,13 @@ fn main() {
         .version("0.1")
         .author("Lyndsey R. M. Dickson")
         .about("Runs a single command in a container")
+        /*
         .arg(clap::Arg::with_name("build")
              .short("b")
              .long("build")
              .help("Build the selected image")
              .takes_value(false))
+             */
         .arg(clap::Arg::with_name("image")
              .short("i")
              .long("image")
@@ -37,24 +59,26 @@ fn main() {
              .index(1))
         .get_matches();
 
-    // Get the image being used
-    let codo_config = &config::get_codo_config()[0];
-    let image_name = match matches.value_of("image") {
-        Some(value) => value,
-        None => {
-            codo_config[config::DEFAULT_IMAGE]
-                .as_str()
-                .expect("Failed to get default image")
-        }
-    };
-    debug!("Image: {:?}", image_name);
-
     // Get the command to be run
-    let mut input_command: Vec<&str> = match matches.values_of("COMMAND") {
-        Some(values) => values.collect(),
-        None => Vec::new()
+    let args: Vec<String> = env::args().collect();
+    let mut input_command: Vec<&str> = Vec::new();
+    let input_command_index = match matches.index_of("COMMAND") {
+        Some(index) => {
+            input_command = vec![];
+            for s in args[index..].iter() { input_command.push(s.as_str()); }
+            index
+        },
+        None => args.len(),
     };
     debug!("Input command: {:?}", input_command);
+
+    // Get the image being used
+    let codo_config = config::get_codo_config();
+    let default_image_name = codo_config[config::DEFAULT_IMAGE]
+                .as_str()
+                .expect("Failed to get default image");
+    let image_name = get_flag_value(&matches, input_command_index, "image", default_image_name);
+    debug!("Image: {:?}", image_name);
 
     // Check if any arguments were passed
     if input_command.len() < 1 {
@@ -66,30 +90,24 @@ fn main() {
     let mut command_contents: Vec<&str> = vec!["sudo", "docker", "run", "-ti", "--rm"];
 
     // Add binding to working directory
-    let mut bind_working_dir = true;
-    let working_dir = match env::current_dir() {
+    let bind_param: String;
+    match env::current_dir() {
         Ok(ok) => match ok.into_os_string().into_string() {
-            Ok(ok) => ok,
+            Ok(working_dir) => {
+                command_contents.push("-v");
+                bind_param = format!("{}:/codo", working_dir);
+                command_contents.push(bind_param.as_str());
+                command_contents.push("-w");
+                command_contents.push("/codo");
+            },
             Err(err) => {
                 error!("Failed to get working directory: {:?}", err);
-                bind_working_dir = false;
-                "".to_string()
             }
         },
         Err(err) => {
             error!("Failed to get working directory: {:?}", err);
-            bind_working_dir = false;
-            "".to_string()
         }
     };
-    let bind_param: String;
-    if bind_working_dir {
-        command_contents.push("-v");
-        bind_param = format!("{}:/codo", working_dir);
-        command_contents.push(bind_param.as_str());
-        command_contents.push("-w");
-        command_contents.push("/codo");
-    }
 
     // Add the image name
     let full_image_name = image_name;
